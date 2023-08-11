0419808de3aaa2c7ba7d7ef25cf2035c5e86bf3f
Ewan Brown <131010184+ebrown462@users.noreply.github.com>
2023-08-11 22:18:50 +0100

    Update discord_communication.py

--- discord_communication.py
+++ discord_communication.py
@@ -1,21 +1,30 @@
+# This script runs a discord bot that transfers a line of text (or other variable) to another PC running a different discord bot.
+
 import discord
 from discord.ext import commands, tasks
 
+# ID and token
 ID = "YOU ID HERE"
 token = "YOUR TOKEN HERE"
+
+# As of discord.py 2.3.2 you need to enable indents
 intents = discord.Intents.all()
 intents.members = True
 intents.presences = True
+
+# Sets prefix to ! (this has to be different from whatever the other bot is using)
 client = commands.Bot(command_prefix='!', intents=intents)
 
+# Startup text
 @client.event
 async def on_ready():
     print("Bot Started")
 
+
 @client.event
 async def on_message(message):
     testData = 'INSERT SECRET DATA HERE'
-    if message.author.id == 708824298177167481 and message.content.startswith('TRANSFER PROTOCOL'):
+    if message.author.id == "OTHER BOT'S ID" and message.content.startswith('TRANSFER PROTOCOL'):
         await message.channel.send(testData)
     await client.process_commands(message)
 
